7b18b1976ee71ccc8a9f1ab13020de39240f26bb
zqiu <zqiu1994@gmail.com>
2018-01-11 15:06:51 -0500

    python parsing file

---
+++ parse.py
@@ -0,0 +1,16 @@
+import sys
+if len(sys.argv) != 3:
+	print "need file and port"
+else:
+	r = open(sys.argv[1])
+	w = open("result"+sys.argv[1],"w")
+	for line in r:
+		towrite = ""
+		if sys.argv[2] == 80:
+			towrite = "http://"
+		else:
+			towrite = "https://"
+		towrite += line[:-1]
+		if int(sys.argv[2]) != 80 and (sys.argv[2]) != 443:
+			towrite += ":"+str(sys.argv[2])
+		w.write(towrite+"\n")
